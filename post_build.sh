060537781412558dfd341edd76e04cc4ab8a0f12
Joel Gilley <gilleyj@gmail.com>
2017-01-12 13:56:45 -0500

    Build Args is a define not assign

--- post_build.sh
+++ post_build.sh
@@ -2,5 +2,6 @@
 
 echo "Attempting build with $APP_ENV"
 echo $APP_ENV > /webroot/web/app_env
-/usr/bin/php ./composer.phar install --no-interaction --no-dev --no-scripts --no-progress -vvv -o
+echo "not building because testing without"
+# /usr/bin/php ./composer.phar install --no-interaction --no-dev --no-scripts --no-progress -vvv -o
 echo "end build with $APP_ENV"
